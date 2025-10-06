dcfa58392c88c3b679cc9e0e9e37d3acb1674bc7
ashrafomid2025 <omidmashraf@gmail.com>
2025-10-06 07:29:07 +0430

    service page

--- src/pages/Exchange.tsx
+++ src/pages/Exchange.tsx
@@ -1,3 +1,10 @@
+import { useEffect } from "react";
+const API_KEY = "";
+const URL = "";
+
 export default function Exchange() {
+  useEffect(() => {
+    fetch("");
+  }, []);
   return <div>Exchange Page</div>;
 }
