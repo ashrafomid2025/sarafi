2d91bb5e36deb425442c33f9cb9a1cf849eac519
ashrafomid2025 <omidmashraf@gmail.com>
2025-10-06 07:36:07 +0430

    service page

--- src/pages/Exchange.tsx
+++ src/pages/Exchange.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
-const API_KEY = "";
-const URL = "";
+const API_KEY: string = "08cae4d0b79be5000a1e0a57";
+const URL: string = "https://v6.exchangerate-api.com/v6/";
 
 export default function Exchange() {
   useEffect(() => {
-    fetch("");
+    fetch(`${URL}${API_KEY}/latest/USD`);
   }, []);
   return <div>Exchange Page</div>;
 }
