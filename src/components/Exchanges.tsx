ac21439412f7602f01e6bc28fdb0e09c42f68426
ashrafomid2025 <omidmashraf@gmail.com>
2025-10-19 19:52:32 +0430

    changes in features

--- src/components/Exchanges.tsx
+++ src/components/Exchanges.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 const URL = "https://v6.exchangerate-api.com/v6/";
 const API_KEY = "08cae4d0b79be5000a1e0a57";
 
@@ -7,30 +7,33 @@ export default function Exchanges() {
   const [to, setTo] = useState("USD");
   const [input, setInput] = useState(0);
   const [output, setoutput] = useState(0);
+  const [isPending, startTransition] = useTransition();
   useEffect(() => {
-    async function fetchData() {
+    function fetchData() {
       if (!from || !to) return;
       // try catch block
-      try {
-        const response = await fetch(`${URL}${API_KEY}/latest/${from}`);
-        const data = await response.json();
-        const rate = data.conversion_rates[to];
-        setoutput(rate * input);
-      } catch (err) {
-        console.log(err);
-      }
+      startTransition(async () => {
+        try {
+          const response = await fetch(`${URL}${API_KEY}/latest/${from}`);
+          const data = await response.json();
+          const rate = data.conversion_rates[to];
+          setoutput(rate * input);
+        } catch (err) {
+          console.log(err);
+        }
+      });
     }
     fetchData();
   }, [from, to, input]);
 
   return (
-    <div className="lg:min-h-screen my-8 w-full p-4 flex justify-center items-center">
+    <div className=" my-8 max-w-3xl mx-auto p-4 flex justify-center items-center">
       {/* card */}
-      <div className="border border-brand/60 bg-white  hover:border-brand transition-all duration-300 hover:-translate-y-1 hover:shadow-xl rounded-md p-4">
-        <h1 className="text-center py-8 font-bold text-2xl">
-          Exchange any currency here
+      <div className="border bg-white  transition-all duration-300  hover:shadow-xl rounded-md p-4">
+        <h1 className="text-center py-3 font-bold text-2xl">
+          Exchange Rates & Currency Converter
         </h1>
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-2 my-5">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-5">
           <div className="">
             <div className="w-full flex flex-col gap-2 items-center">
               <label htmlFor="from">From</label>
@@ -38,7 +41,7 @@ export default function Exchanges() {
                 onChange={(e) => setFrom(e.target.value)}
                 id="from"
                 value={from}
-                className="border focus:outline-0 rounded-xs py-1  w-10/12"
+                className="border focus:outline-0 rounded-xs py-1 w-full"
               >
                 <option value="AFN">Afghani</option>
                 <option value="USD">Dollor USD</option>
@@ -62,9 +65,9 @@ export default function Exchanges() {
               <input
                 value={input}
                 onChange={(e) => setInput(Number(e.target.value))}
-                className="border py-1 px-4 w-10/12 rounded-xs focus:outline-0"
+                className="border py-1 px-4 w-full rounded-xs focus:outline-0"
                 placeholder="Enter the amount"
-                type="number"
+                type="text"
               />
             </div>
           </div>
@@ -75,7 +78,7 @@ export default function Exchanges() {
                 id="to"
                 onChange={(e) => setTo(e.target.value)}
                 value={to}
-                className="border focus:outline-0 py-1 px-4 w-10/12"
+                className="border focus:outline-0 py-1 px-4 w-full"
               >
                 <option value="AFN">Afghani</option>
                 <option value="USD">Dollor USD</option>
@@ -98,9 +101,9 @@ export default function Exchanges() {
               <label htmlFor="">Equals</label>
               <input
                 readOnly
-                value={output}
+                value={isPending ? "Please Wait" : output}
                 onChange={(e) => setoutput(+e.target.value)}
-                className="border py-1 px-4 w-10/12 rounded-xs focus:outline-0"
+                className="border py-1 px-4 w-full rounded-xs focus:outline-0"
                 type="number"
               />
             </div>
