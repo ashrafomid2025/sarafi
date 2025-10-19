5b28290e00a4a2a8de159191dd539bb9d118154b
ashrafomid2025 <omidmashraf@gmail.com>
2025-10-19 19:13:32 +0430

    changes in features

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,62 +1,73 @@
-import { useEffect, useState } from "react";
-
-import { Button } from "./ui/button";
-
+import { Button } from "@/components/ui/button";
+import { useEffect, useState, useTransition } from "react";
+import { motion } from "framer-motion";
 const API_KEY = "88a68df397d7dd162099b2864d17492f";
-const URL = `https://gnews.io/api/v4/search?q=Business&lang=en&max=10&apikey=${API_KEY}`;
-type News = {
-  id: string;
-  title: string;
-  description: string;
-  content: string;
-  url: string;
-  image: string;
-  lang: string;
-  publishedAt: string;
-};
-
+const URL = `https://gnews.io/api/v4/search?q=Business&lang=en&max=12&apikey=${API_KEY}`;
 export default function Articles() {
-  const [news, setNews] = useState<News[]>([]);
+  type Article = {
+    id: string;
+    title: string;
+    description: string;
+    content: string;
+    url: string;
+    image: string;
+    publishedAt: string;
+  };
+  const [news, setNews] = useState<Article[]>([]);
+  const [isPending, startTransition] = useTransition();
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(URL);
-      const data = await response.json();
-      setNews(data.articles);
+    function fetchData() {
+      startTransition(async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setNews(data.articles);
+      });
     }
     fetchData();
   }, []);
   return (
     <div className="py-24">
-      <h1 className="text-center font-bold text-4xl my-8">
-        Our Latest Articles
+      <h1 className="text-center text-5xl uppercase my-5 font-bold">
+        Big Headlines of Business
       </h1>
-      <div className="w-full grid grid-cols-2 gap-3">
-        {news.map((item) => (
-          <div
-            key={item.id}
-            className="bg-white overflow-hidden border rounded-md h-96"
-          >
-            <img
-              src={item.image}
-              className="w-full h-44 rounded-t-md"
-              alt={item.id}
-            />
-            <div className="flex justify-between p-4">
-              <span className="text-brand">
-                {item.publishedAt.substring(0, 10)}
-              </span>
-              <Button
-                variant="outline"
-                className="rounded-none bg-brand text-white"
-              >
-                Read More
-              </Button>
-            </div>
-            <h1 className="text-xl p-2 font-bold">{item.title}</h1>
-            <p className="p-2 overflow-hidden">{item.description}</p>
-          </div>
-        ))}
-      </div>
+      {isPending ? (
+        <div className="h-48 w-full flex justify-center items-center">
+          <h1>Loading, Please wait</h1>
+        </div>
+      ) : (
+        <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 p-2 gap-2">
+          {news.map((item, index) => (
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ amount: 0.4, once: false }}
+              transition={{ duration: 0.8, delay: 0.2 * index }}
+              id={item.id}
+              className="border h-96 overflow-hidden rounded-md bg-white"
+            >
+              <div className="relative w-full h-full">
+                <img
+                  src={item.image}
+                  alt={item.id}
+                  className="w-full h-full object-cover"
+                />
+                <div className="absolute left-0 bottom-0 h-5/12 overflow-y-hidden bg-black/60 p-2 w-full">
+                  <h1 className="text-white font-bold text-2xl h-28">
+                    {item.title}
+                  </h1>
+                  <div className="flex justify-end items-end">
+                    <Button className="bg-white text-black hover:bg-white hover:border hover:-translate-y-1 hover:shadow-2xl transition-all duration-300 hover:text-black rounded-xs">
+                      <a href={item.url} target="_blank">
+                        Read More
+                      </a>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
